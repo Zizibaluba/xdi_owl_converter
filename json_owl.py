5feb5e6a9c4965755c00e17cb94a958741815664
Brian Wu <brian.wu@neustar.biz>
2014-03-11 18:18:44 -0500

    Past initial stage
    About to start writing regex for owl conversion.

--- json_owl.py
+++ json_owl.py
@@ -1,6 +1,7 @@
 import json
 import sys
 import pprint
+import re
 
 
 def pprintJson(data):
@@ -9,7 +10,7 @@ def pprintJson(data):
 	print
 
 def owlGenerate(data):
-	
+	print
 
 if __name__ == '__main__':
 	filename = sys.argv[1]
@@ -17,7 +18,18 @@ if __name__ == '__main__':
 	json_data = open(filename)
 	data = json.load(json_data)
 
+	# prints a pretty version of the json file before beginning
 	pprintJson(data)
 
 	owlGenerate(data)
+
+	# prints the number of "primary keys in the json file"
 	print len(data.keys())
+	
+	# prints key:value pairs for the entirety of the json at the n+1 level
+	j= 0
+	for i in data.keys():
+		j += 1
+		print "KEY " + str(j) + ": " + str(i)
+		print "VALUE " + str(j) + ": " + str(data[i]) + "\n"
+
